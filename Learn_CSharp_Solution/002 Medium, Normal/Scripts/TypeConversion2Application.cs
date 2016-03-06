4425c7991fd7b46150a3891f42d73a445791f897
AppleJack1113 <lolmaker4444@live.nl>
2016-03-06 17:05:47 +0100

    Added files via upload
    The Solution with the projects in it. Will be updated everytime I complete an C# Script.

---
+++ Learn_CSharp_Solution/002 Medium, Normal/Scripts/TypeConversion2Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TypeConversion2Application
+{
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            int i = 75;
+
+            float f = 53.00f;
+
+            double d = 2345.7562;
+
+            bool b = true;
+
+
+            Console.WriteLine(i.ToString());
+
+            Console.WriteLine(f.ToString());
+
+            Console.WriteLine(d.ToString());
+
+            Console.WriteLine(b.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Learn_CSharp_Solution/Learn_CS/Files/Scripts/HelloWorldApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] Args)
+        {
+            /* My First Program In C# */
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
